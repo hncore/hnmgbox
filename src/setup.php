eb71959080bfc8b5dd1c7f9e150f8e77fe226711
hncore <hncore@icloud.com>
2024-10-28 07:21:16 +0700

    update

---
+++ src/setup.php
@@ -0,0 +1,40 @@
+<?php
+use HNMG\LoadBox\LB_Metabox;
+use HNMG\LoadBox\LB_AdminPage;
+use HNMG\LoadBox\LB_LoadBox;
+
+function hnmgbox_get_all(){
+  return LB_LoadBox::get_all_hnmgboxs();
+}
+
+
+function hnmgbox_get( $hnmgbox_id ){
+  return LB_LoadBox::get( $hnmgbox_id );
+}
+
+
+function hnmgbox_new_metabox( $options = array() ){
+  return new LB_Metabox( $options );
+}
+
+
+function hnmgbox_new_admin_page( $options = array() ){
+  return new LB_AdminPage( $options );
+}
+
+
+function hnmgbox_get_field_value( $hnmgbox_id, $field_id = '', $default = '', $post_id = '' ){
+  return LB_LoadBox::get_field_value( $hnmgbox_id, $field_id, $default, $post_id );
+}
+
+add_shortcode( 'hnmgbox_get_field_value', 'hnmgbox_get_field_value_shortcode' );
+function hnmgbox_get_field_value_shortcode( $atts ) {
+    $a = shortcode_atts( array(
+        'hnmgbox_id' => null,
+        'field_id' => '',
+        'default' => '',
+        'post_id' => '',
+    ), $atts );
+    return hnmgbox_get_field_value( $a['hnmgbox_id'], $a['field_id'], $a['default'], $a['post_id'] );
+}
+
